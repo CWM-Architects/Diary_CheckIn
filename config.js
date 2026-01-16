334fae1b82039f9bb3cbc3fff67210a4a1f1410b
CWM-Architects <markngo00707@gmail.com>
2026-01-16 10:47:32 +0800

    Refactor API_CONFIG and update redirectUrl
    Updated redirectUrl to use lowercase domain.

--- config.js
+++ config.js
@@ -5,10 +5,11 @@ const API_CONFIG = {
   apiUrl: "https://script.google.com/macros/s/AKfycbynjVkSQgmEXNNe8SHnlWkeVGzyAQeA5RMJxwgsMw/dev",
 
   // 新增回呼網址
-  redirectUrl: "https://CWM-Architects.github.io/Diary_CheckIn/"
+  redirectUrl: "https://cwm-architects.github.io/Diary_CheckIn/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.4.1'
 };
 // 👇 新增：為了兼容性，同時定義全域變數 apiUrl
+
 const apiUrl = API_CONFIG.apiUrl;
